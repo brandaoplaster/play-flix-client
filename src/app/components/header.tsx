a2225e123551ad6a26e36f68438e504c1590bfac
brandaoplaster <brandaoplaster@gmail.com>
2024-04-24 19:25:53 -0300

    feat: Add profile in header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className='fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 lg:px-10 lg:py-6'>
-      <div className='flex items-center space-x-2 md:space-x-4'>
+    <header className='fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6'>
+      <div className='flex items-center space-x-4 md:space-x-4'>
         <img src='/images/logo.svg' alt='Netflix' className='w-24' />
 
         <ul className='hidden space-x-4 text-sm font-semibold md:flex'>
@@ -12,6 +12,15 @@ export default function Header() {
           <li>Movies</li>
           <li>Latest</li>
         </ul>
+      </div>
+
+      <div className='flex items-center space-x-4'>
+        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+        <img
+          src='https://rb.gy/g1pwyx'
+          alt='Avatar'
+          className='cursor-pointer rounded'
+        />
       </div>
     </header>
   );
