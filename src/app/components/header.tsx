9f0c720e76cef5ad999f70197bb19f2f644b31d8
brandaoplaster <brandaoplaster@gmail.com>
2024-04-25 23:49:05 -0300

    feat: Adding components modular

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,4 +1,5 @@
 'use-client';
+import Image from 'next/image';
 import React, { useEffect } from 'react';
 
 export default function Header() {
@@ -18,6 +19,38 @@ export default function Header() {
     };
   }, []);
 
+  const Logo = () => (
+    <Image
+      src='/images/logo.svg'
+      alt='play flix'
+      width={80}
+      height={80}
+      className='cursor-pointer'
+    />
+  );
+
+  const NavLinks = () => (
+    <nav>
+      <ul className='hidden space-x-4 text-sm font-semibold md:flex'>
+        <li>Home</li>
+        <li>TV Show</li>
+        <li>Movies</li>
+        <li>Latest</li>
+      </ul>
+    </nav>
+  );
+
+  const UserProfile = () => (
+    <div className='flex items-center space-x-4'>
+      <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
+      <Image
+        src='https://rb.gy/g1pwyx'
+        alt='Avatar'
+        className='cursor-pointer rounded'
+      />
+    </div>
+  );
+
   return (
     <header
       className={`${
@@ -25,24 +58,12 @@ export default function Header() {
       } fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}
     >
       <div className='flex items-center space-x-4 md:space-x-4'>
-        <img src='/images/logo.svg' alt='Netflix' className='w-24' />
+        <Logo />
 
-        <ul className='hidden space-x-4 text-sm font-semibold md:flex'>
-          <li>Home</li>
-          <li>TV Show</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+        <NavLinks />
       </div>
 
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <img
-          src='https://rb.gy/g1pwyx'
-          alt='Avatar'
-          className='cursor-pointer rounded'
-        />
-      </div>
+      <UserProfile />
     </header>
   );
 }
