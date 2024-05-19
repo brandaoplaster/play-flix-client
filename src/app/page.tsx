f2b3090cb711d42ca2f3084baa0cb5de3b3581e4
brandaoplaster <brandaoplaster@gmail.com>
2024-05-19 15:38:24 -0300

    feat: Adding movies rows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,12 +33,18 @@ export default function Home() {
         </div>
 
         <div className='flex-col space-y-4'>
-          <div className='flex space-x-4 overflow-x-scroll'>
-            {[1, 2, 3, 4, 5].map((index) => (
+          <div className='flex'>
+            <h2 className='-ml-2 inline-flex items-center text-2xl font-bold'>
+              Feature
+            </h2>
+          </div>
+          <div className='-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide'>
+            {[1, 2, 3, 4, 5, 5, 5, 5, 5].map((index) => (
               <div
                 key={index}
-                className='group relative h-28 min-w-[200]
-                transition duration-200 ease-in hover:z-50 hover:scale-110
+                className='group relative h-28 min-w-[200px] transform bg-gradient-to-t from-transparent
+                to-black transition duration-200 ease-in hover:z-50 hover:scale-110
+                md:h-40 md:min-w-[300px] lg:h-52 lg:min-w-[400px]
               '
               >
                 <Image
