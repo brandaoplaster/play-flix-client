690b70754b052472a406b5d85001aafbcd4458fe
brandaoplaster <brandaoplaster@gmail.com>
2024-05-19 15:57:37 -0300

    feat: Adding component movieRow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
 import Header from './components/Header';
 import { InformationCircleIcon, PlayIcon } from '@heroicons/react/16/solid';
+import { MovieRow } from './components/MovieRow';
 
 export default function Home() {
   return (
@@ -32,32 +32,9 @@ export default function Home() {
           </button>
         </div>
 
-        <div className='flex-col space-y-4'>
-          <div className='flex'>
-            <h2 className='-ml-2 inline-flex items-center text-2xl font-bold'>
-              Feature
-            </h2>
-          </div>
-          <div className='-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide'>
-            {[1, 2, 3, 4, 5, 5, 5, 5, 5].map((index) => (
-              <div
-                key={index}
-                className='group relative h-28 min-w-[200px] transform bg-gradient-to-t from-transparent
-                to-black transition duration-200 ease-in hover:z-50 hover:scale-110
-                md:h-40 md:min-w-[300px] lg:h-52 lg:min-w-[400px]
-              '
-              >
-                <Image
-                  src={`/item_${index}.png`}
-                  alt='image'
-                  width={200}
-                  height={300}
-                  className='rounded'
-                />
-              </div>
-            ))}
-          </div>
-        </div>
+        <MovieRow sectionTitle='Trending Now' />
+        <MovieRow sectionTitle='Top Rated' />
+        <MovieRow sectionTitle='Action Movies' />
       </main>
     </div>
   );
