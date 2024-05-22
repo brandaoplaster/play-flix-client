849267c5df99528d82d236595da0b750015b686c
brandaoplaster <brandaoplaster@gmail.com>
2024-05-21 23:32:57 -0300

    feat: Adding input form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -15,6 +15,37 @@ export default function Login() {
               </a>
             </p>
           </div>
+          <div className='mt-8 flex flex-col space-y-4'>
+            <div className='flex flex-col space-y-1'>
+              <label
+                htmlFor='email'
+                className='text-sm font-semibold text-gray-500'
+              >
+                email
+              </label>
+              <input
+                type='text'
+                name='email'
+                placeholder='Enter your email'
+                className='rounded-lg border-gray-600 bg-gray-700 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500'
+              />
+            </div>
+
+            <div className='flex flex-col space-y-1'>
+              <label
+                htmlFor='password'
+                className='text-sm font-semibold text-gray-500'
+              >
+                password
+              </label>
+              <input
+                type='password'
+                name='password'
+                placeholder='Password'
+                className='rounded-lg border-gray-600 bg-gray-700 px-4 py-2 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-red-500'
+              />
+            </div>
+          </div>
         </form>
       </div>
     </div>
