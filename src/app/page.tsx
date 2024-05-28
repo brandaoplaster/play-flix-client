d5fa3c77cd0fc5bf846277bc47412ceb755d3e55
brandaoplaster <brandaoplaster@gmail.com>
2024-05-28 18:20:03 -0300

    feat: Adding genres in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 import { Banner } from '@/app/components/Banner';
 import { MovieRow } from '@/app/components/MovieRow';
-import { getFeaturedMovies } from './services/MovieService';
+import { getFeaturedMovies, getMoviesByGenre } from './services/MovieService';
 import Header from './components/Header';
 
 async function Home() {
   const featuredMovie = await getFeaturedMovies('106');
+
+  const genres = ['Drama', 'Action', 'Comedy', 'Animation'];
+
+  const movies = await Promise.all(
+    genres.map(async (genre) => {
+      const movies = await getMoviesByGenre(genre, { _limit: 8 });
+      return { sectionTitle: genre, movies };
+    })
+  );
 
   return (
     <div className='relative bg-gradient-to-b pb-8'>
       <Header />
       <main className='relative overflow-y-scroll p-8 pb-20 scrollbar-hide lg:pl-16 '>
         <Banner movie={featuredMovie} />
-        <MovieRow sectionTitle='Trending Now' />
-        <MovieRow sectionTitle='Top Rated' />
-        <MovieRow sectionTitle='Action Movies' />
+        {movies.map((movie) => (
+          <MovieRow
+            key={movie.sectionTitle}
+            sectionTitle={movie.sectionTitle}
+            movies={movie.movies}
+          />
+        ))}
       </main>
     </div>
   );
