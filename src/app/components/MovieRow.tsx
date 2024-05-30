bd2724cb718467c4ce97402921d881b1975b41cf
brandaoplaster <brandaoplaster@gmail.com>
2024-05-29 23:16:24 -0300

    refactor: Refactoring movieRow component

--- src/app/components/MovieRow.tsx
+++ src/app/components/MovieRow.tsx
@@ -1,36 +1,32 @@
-import Image from 'next/image';
-import { Movie, Movies } from '../types/Movie';
+import { Movie as MovieType } from '@/app/types/Movie';
+import { MovieCard } from './MovieCard';
 
-type MovieRowProps = {
+interface MovieRowProps {
   sectionTitle: string;
-  movies: Movies;
-};
+  movies: MovieType[];
+}
 
-export const MovieCard = ({ movie }: { movie: Movie }) => (
-  <div className='group relative min-h-[12vh] rounded bg-zinc-900 md:min-h-[12vw]'>
-    <Image
-      src={movie.bannerFileURL}
-      alt={movie.title}
-      width={600}
-      height={400}
-      className='rounded-md object-cover object-top transition'
-    />
-  </div>
-);
+export interface MovieCardProps {
+  movie: MovieType;
+}
 
-export function MovieRow({ sectionTitle, movies }: MovieRowProps) {
+export const MovieRow: React.FC<MovieRowProps> = async ({
+  sectionTitle,
+  movies,
+}) => {
   return (
-    <div className='flex-col space-y-4'>
+    <div className='flex-col space-y-2'>
       <div className='flex'>
-        <h2 className='-ml-2 inline-flex items-center text-2xl font-bold'>
+        <h2 className='my-4 inline-flex items-center text-2xl font-bold'>
           {sectionTitle}
         </h2>
       </div>
+
       <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8'>
-        {movies.map((movie, index) => (
+        {movies.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
   );
-}
+};
